2d47c4e1aa9e7f11303617c85d563837f067232e
Robert Bossaert <contact@robertbossaert.com>
2024-12-22 09:48:07 +0100

    (feat): progress
    (feat): update deploy workflow
    
    Create CNAME
    
    (chore): change output directory
    
    (chore): change output directory
    
    (chore): change output directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,12 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
 export default function (eleventyConfig) {
   eleventyConfig.setServerOptions({
-    watch: ['_site/**/*.css'],
+    watch: ['docs/**/*.css'],
   });
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     extensions: "html",
@@ -18,6 +21,7 @@ export default function (eleventyConfig) {
   return {
     dir: {
       input: 'src',
+      output: 'docs'
     },
   };
 };
